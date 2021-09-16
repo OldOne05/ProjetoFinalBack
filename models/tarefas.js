dbea4e43d19f3b67123e950ca238970176611008
Cauã <cauacampos05@gmail.com>
2021-09-15 22:14:47 -0300

    adição de novos campos

--- models/tarefas.js
+++ models/tarefas.js
@@ -5,6 +5,8 @@ const tarefaModel = new mongoose.Schema({
     descricao: { type: String, required: true },
     prioridade: { type: String, required: true },
     status: { type: String, required: true },
+    prazo: { type: Date, required: true },
+    dataCriacao: { type: Date, default: Date.now }
 })
 
 const Tarefa = mongoose.model("tarefa", tarefaModel);
